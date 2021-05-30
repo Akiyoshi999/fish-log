041dbbc67efd5ba991bb32de4e74dd85b3bcc940
Akiyoshi <zundokokiyoshi999@gmail.com>
2021-05-30 21:11:44 +0900

    #11 ユーザー登録テスト追加

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * デフォルトのシーダーが各テスト前に実行するか
+     *
+     * @var boolean
+     */
+    protected $seed = true;
 }
